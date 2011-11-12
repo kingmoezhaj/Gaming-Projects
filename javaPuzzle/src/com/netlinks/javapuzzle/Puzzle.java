7a278d86ee513b416c56be765ea0581e5becc116
Wadi Chemkhi <wadi3lamda@gmail.com>
2011-11-13 00:41:22 +0100

    added a apicture an few library imports plus variables
    Signed-off-by: Wadi Chemkhi <wadi3lamda@gmail.com>

--- javaPuzzle/src/com/netlinks/javapuzzle/Puzzle.java
+++ javaPuzzle/src/com/netlinks/javapuzzle/Puzzle.java
@@ -1,12 +1,19 @@
 package com.netlinks.javapuzzle;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Puzzle implements ApplicationListener {
+	SpriteBatch spriteBatch;
+	Texture texture;
+	BitmapFont font;
+	
 
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
+	
 		
 	}
 
