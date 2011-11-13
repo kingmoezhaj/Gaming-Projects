ee70189c5231fd853e6b4afefcf33467f32670ad
Wadi Chemkhi <wadi3lamda@gmail.com>
2011-11-13 02:07:18 +0100

    a working example of drawing a certain rectangle of an image to a precise rectangle on the screen
    Signed-off-by: Wadi Chemkhi <wadi3lamda@gmail.com>

--- javaPuzzle/src/com/netlinks/javapuzzle/Puzzle.java
+++ javaPuzzle/src/com/netlinks/javapuzzle/Puzzle.java
@@ -1,6 +1,9 @@
 package com.netlinks.javapuzzle;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -13,6 +16,10 @@ public class Puzzle implements ApplicationListener {
 
 	@Override
 	public void create() {
+		font = new BitmapFont();
+		font.setColor(Color.RED);
+		texture = new Texture(Gdx.files.internal("data/img.jpg"));
+		spriteBatch = new SpriteBatch();
 	
 		
 	}
@@ -28,11 +35,16 @@ public class Puzzle implements ApplicationListener {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
 	@Override
 	public void render() {
-		// TODO Auto-generated method stub
 		
+		Gdx.graphics.getGL10().glClear(GL10.GL_COLOR_BUFFER_BIT);
+		spriteBatch.begin();
+		spriteBatch.setColor(Color.WHITE);
+		spriteBatch.draw(texture, 0, 0, 100,100, 0, 0, 100, 100, false, false); 
+		//draw(Texture texture, float x, float y, float width, float height, int srcX, int srcY, int srcWidth, int srcHeight, boolean flipX, boolean flipY) 
+		spriteBatch.end();
 	}
 
 	@Override
